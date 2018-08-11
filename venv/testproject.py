23f43df0f37b965582cdd179691b7eb894b04bb7
Nikhil-Bajaj <nikhilbajaj1474@gmail.com>
2018-08-11 13:59:54 +0530

    Project1

--- venv/testproject.py
+++ venv/testproject.py
@@ -1,8 +1,31 @@
-import pandas as pd
 import requests
-url = "http://howstat.com/cricket/Statistics/Players/PlayerCountryList.asp?Country=IND"
-response = requests.get(url)
-#print(response.text)
+from bs4 import BeautifulSoup
+import html5lib
+import requests
+from bs4 import BeautifulSoup
+import os
+def make_soup(URL):
+    r = requests.get(URL)
+    soupdata = BeautifulSoup(r.content, 'html.parser')
+    return soupdata
 
-data = pd.read_html(url)
-print(data)
+playerdata=playerdatasaved=""
+soup = make_soup("http://howstat.com/cricket/Statistics/Players/PlayerCountryList.asp?Country=IND")
+#x=[]
+for record in soup.findAll('tr'):
+    #print(record.text)
+    playerdata=""
+    #print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    for data in record.findAll('td'):
+
+        #print(data.text)
+        playerdata = playerdata +","+data.text
+        if len(playerdata)!=0:
+            playerdatasaved = playerdatasaved + playerdata[1:]
+header = "Name,	TM,	TRuns,	TBatAvg,	TWkts,	TBowlAvg,	OM,	ORuns,	OBatAvg,OWkts,OBowlAvg,T2M,	T2Runs,T2BatAvg,T2Wkts,T2Bowl Avg"
+file = open(os.path.expanduser("INDIA PLAYERS.csv"),"wb")
+file.write(bytes(header,encoding="ascii",errors="ignore"))
+
+file.write(bytes(playerdatasaved,encoding="ascii",errors="ignore"))
+#print(playerdatasaved)
+
